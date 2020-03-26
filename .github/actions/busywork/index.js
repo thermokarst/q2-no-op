de3650c52394ecf48f52e1e6b2591f60eeab6649
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-26 11:16:00 -0700

    typos

--- .github/actions/busywork/index.js
+++ .github/actions/busywork/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 try {
-    await exec.exec('echo "hello world"');
+  exec.exec('echo "hello world"');
 } catch (error) {
   core.setFailed(error.message);
 }
