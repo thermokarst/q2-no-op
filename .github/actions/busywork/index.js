cd7e0165682a8eaffa2effcc623c7fd1c6b593ba
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-27 11:20:19 -0700

    hmm

--- .github/actions/busywork/index.js
+++ .github/actions/busywork/index.js
@@ -5,10 +5,10 @@ const exec = require('@actions/exec');
 try {
   const opts = {cwd: '.github/actions/busywork'};
 
-  exec.exec('./install_miniconda.sh', [], opts)
+  await exec.exec('./install_miniconda.sh', [], opts)
   // TODO: how to error if the exec above fails?
 
-  exec.exec('./build_package.sh', [], opts)
+  await exec.exec('./build_package.sh', [], opts)
   // TODO: how to error if the exec above fails?
 
 } catch (error) {
