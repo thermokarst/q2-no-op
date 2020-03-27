7d55604d29c358d0aefdc4dae2a4678b08089d6d
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-27 10:58:53 -0700

    gha groups

--- .github/actions/busywork/index.js
+++ .github/actions/busywork/index.js
@@ -4,9 +4,16 @@ const exec = require('@actions/exec');
 
 try {
   const opts = {cwd: '.github/actions/busywork'};
+
+  core.startGroup('install miniconda')
   exec.exec('./install_miniconda.sh', [], opts)
   // TODO: how to error if the exec above fails?
+  core.endGroup()
+
+  core.startGroup('verify install')
   exec.exec('conda info');
+  core.endGroup()
+
 } catch (error) {
   core.setFailed(error.message);
 }
