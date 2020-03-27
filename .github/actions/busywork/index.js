2d4548c43ea1a924f8a36b621e9f4a18ac8baac9
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-27 11:22:55 -0700

    promises promises promises

--- .github/actions/busywork/index.js
+++ .github/actions/busywork/index.js
@@ -2,15 +2,19 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 
-try {
-  const opts = {cwd: '.github/actions/busywork'};
+async function main() {
+  try {
+    const opts = {cwd: '.github/actions/busywork'};
 
-  await exec.exec('./install_miniconda.sh', [], opts)
-  // TODO: how to error if the exec above fails?
+    await exec.exec('./install_miniconda.sh', [], opts)
+    // TODO: how to error if the exec above fails?
 
-  await exec.exec('./build_package.sh', [], opts)
-  // TODO: how to error if the exec above fails?
+    await exec.exec('./build_package.sh', [], opts)
+    // TODO: how to error if the exec above fails?
 
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
