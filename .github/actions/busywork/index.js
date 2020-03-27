aaa59d451d846bc601fdca487685556496ef6149
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-27 10:33:18 -0700

    messed up the sig

--- .github/actions/busywork/index.js
+++ .github/actions/busywork/index.js
@@ -4,7 +4,7 @@ const exec = require('@actions/exec');
 
 try {
   const opts = {cwd: '.github/actions/busywork'};
-  exec.exec('./doit.sh', opts)
+  exec.exec('./doit.sh', [], opts)
   // TODO: how to error if the exec above fails?
 } catch (error) {
   core.setFailed(error.message);
