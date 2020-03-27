058e7415204cc75e1235896f38058a695dced2bc
Matthew Dillon <matthewrdillon@gmail.com>
2020-03-27 08:18:24 -0700

    Testing exec

--- .github/actions/busywork/index.js
+++ .github/actions/busywork/index.js
@@ -3,6 +3,10 @@ const exec = require('@actions/exec');
 
 try {
   exec.exec('echo "hello world"');
+  exec.exec('wget',[
+    '-o=foo',
+    'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh',
+  ])
 } catch (error) {
   core.setFailed(error.message);
 }
